d2a0d16b4bc11e31316e8964ea62a8b523adf3aa
uraltermann <maurits.boettger@gmail.com>
2019-08-02 12:23:34 +0200

    add uncommitted changes and files

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,11 @@ const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 const useref = require('gulp-useref'); // js + css concatenation (make 1 file)
 const uglify = require('gulp-uglify'); // js minification
-const cssnano = require('gulp-cssnano'); // css minification
+const cleancss = require('gulp-clean-css'); // css minification
 const gulpIf = require('gulp-if'); // conditionally run a task
 const imagemin = require('gulp-imagemin'); // optimize images (reduce size)
 const cache = require('gulp-cache'); // used for save optimized images in cache
 const del = require('del'); // clean/delete unused files
-const runSequence = require('run-sequence'); // specify sequence of tasks
 const babel = require('gulp-babel'); // transpile es6 to es5 (no module support)
 const nunjucksRender = require('gulp-nunjucks-render'); // template engine
 // postcss
@@ -19,15 +18,16 @@ const flexbugs = require('postcss-flexbugs-fixes'); // flexbox fixes
 /* Development tasks
 –––––––––––––––––––––––––––––––––––––––––––––––––– */
 
-gulp.task('browserSync', () => { // Start browserSync local server
+gulp.task('browserSync', (done) => { // Start browserSync local server
   browserSync.init({
     server: {
       baseDir: 'app',
     },
   });
+  done();
 });
 
-gulp.task('nunjucks', () => { // build html files from templates and partials
+gulp.task('nunjucks', (done) => { // build html files from templates and partials
   gulp.src('app/pages/**/*.+(nunjucks)')
     .pipe(nunjucksRender({ // render templates with nunjucks
       path: ['app/templates'],
@@ -36,11 +36,12 @@ gulp.task('nunjucks', () => { // build html files from templates and partials
     .pipe(browserSync.reload({ // browser refresh
       stream: true,
     }));
+  done();
 });
 
-gulp.task('sass', () => {
+gulp.task('sass', (done) => {
   const processors = [
-    autoprefixer({ browsers: ['Chrome >= 35', 'Firefox >= 38', 'Edge >= 12', 'Explorer >= 10', 'iOS >= 8', 'Safari >= 8', 'Android 2.3', 'Android >= 4', 'Opera >= 12'] }),
+    autoprefixer(),
     flexbugs,
   ];
   gulp.src('app/scss/**/*.scss')
@@ -50,46 +51,46 @@ gulp.task('sass', () => {
     .pipe(browserSync.reload({ // browser refresh
       stream: true,
     }));
+  done();
 });
 
-gulp.task('watch', () => { // Watchers
-  gulp.watch('app/**/*.nunjucks', ['nunjucks']);
-  gulp.watch('app/scss/**/*.scss', ['sass']);
+gulp.task('watch', (done) => { // Watchers
+  gulp.watch('app/**/*.nunjucks', gulp.series('nunjucks'));
+  gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
   // Reload browser whenever JS files change
   gulp.watch('app/js/**/*.js', browserSync.reload);
+  done();
 });
 
 /* Build tasks (Optimization)
 –––––––––––––––––––––––––––––––––––––––––––––––––– */
 
 // Optimize CSS and JavaScript
-gulp.task('useref', () => {
+gulp.task('useref', (done) => {
   gulp.src('app/*.html')
     .pipe(useref()) // concatenate
-    .pipe(gulpIf('*.js', babel({ presets: ['es2015'] }))) // transpile es6 to es5
+    .pipe(gulpIf('*.js', babel({ presets: ['@babel/preset-env'] }))) // transpile es6 to es5
     .pipe(gulpIf('*.js', uglify())) // minify only js files
-    .pipe(gulpIf('*.css', cssnano())) // minify only css files
+    .pipe(gulpIf('*.css', cleancss({ compatibility: 'ie8' }))) // minify only css files
     .pipe(gulp.dest('dist'));
+  done();
 });
 
 // Optimize images
-gulp.task('images', () => {
+gulp.task('images', (done) => {
   gulp.src('app/images/**/*.+(png|jpg|gif|svg|ico)')
     .pipe(cache(imagemin())) // cache minified images (only new/modified images get compressed)
     .pipe(gulp.dest('dist/images'));
+  done();
 });
 
 // Cleaning
-gulp.task('clean:dist', () => { // delete dist folder whenever task is run
+gulp.task('clean:dist', (done) => { // delete dist folder whenever task is run
   del.sync('dist');
+  done();
 });
 
 /* Run sequences
 –––––––––––––––––––––––––––––––––––––––––––––––––– */
-gulp.task('default', (callback) => {
-  runSequence('nunjucks', ['sass', 'browserSync'], 'watch', callback);
-});
-
-gulp.task('build', (callback) => {
-  runSequence('clean:dist', 'nunjucks', 'sass', ['useref', 'images'], callback);
-});
+gulp.task('default', gulp.series('nunjucks', gulp.parallel('sass', 'browserSync'), 'watch'), (done) => { done(); });
+gulp.task('build', gulp.series('clean:dist', 'nunjucks', 'sass', gulp.parallel('useref', 'images')), (done) => { done(); });
